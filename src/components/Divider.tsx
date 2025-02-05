a1e5f778e1f459b012aff70c1d1dbc5b101cdcda
Cauã Campos <campos.eet@gmail.com>
2025-02-05 18:28:37 -0300

    Selector

---
+++ src/components/Divider.tsx
@@ -0,0 +1,7 @@
+import { View } from 'react-native';
+
+export default function Divider() {
+    return (
+        <View className='w-[1.5px] h-[24px] rounded-full bg-gray-300 ml-[16px]'></View>
+    );
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import { Text, View, Image, TextInput  } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { ArrowDownUp } from 'lucide-react-native';
 import logo from "@assets/logo.png"
 
+import Divider from '@components/Divider';
+
+
+import SelectDropdown from 'react-native-select-dropdown';
 
 export default function Home() {
+    const [borderStyle, setBorderStyle] = useState("border border-gray-300");
+    
+    const currencies = [
+        { code: "BRL", flag: "🇧🇷" },
+        { code: "USD", flag: "🇺🇸" },
+        { code: "EUR", flag: "🇪🇺" },
+        { code: "GBP", flag: "🇬🇧" },
+        { code: "CHF", flag: "🇨🇭" },
+        { code: "JPY", flag: "🇯🇵" },
+        { code: "CAD", flag: "🇨🇦" },
+        { code: "CNY", flag: "🇨🇳" },
+        { code: "INR", flag: "🇮🇳" }
+      ];
+
+    const [selectedCurrency, setSelectedCurrency] = useState(currencies[0]);
+
     return (
         <SafeAreaView className='flex-1 w-full items-center justify-center bg-gray-400'>   
             <Image source={logo} className='mt-[40px] w-[140px] h-[32px]'/>
@@ -18,17 +39,47 @@ export default function Home() {
                     </View>
 
                     <View className='w-full mt-[40px] items-center'>
-                        <View className='w-full h-[56px] rounded-xl border border-gray-100'>
-                            
+                        <View className={`w-full h-[56px] py-[16px] pl-[16px] rounded-xl flex-row items-center ${borderStyle}`}>
+                            <TextInput 
+                                className='flex-1 h-[56px] font-regular color-gray-100 text-[15px]'
+                                placeholder='R$ 1.000,00'
+                                placeholderTextColor="#0F172A"
+                                // onChangeText={setTaskName}
+                                // value={taskName}
+                                onFocus={() => setBorderStyle("border-[1.5px] border-purple-base")}
+                                onBlur={() => setBorderStyle("border border-gray-300")}
+                            />
+
+                            <Divider />
+
+                            <SelectDropdown
+                                data={currencies}
+                                defaultValue={selectedCurrency}
+                                dropdownStyle={{borderRadius: 10, marginTop: -30, height: 200}}
+                                onSelect={(selectedItem) => setSelectedCurrency(selectedItem)}
+                                renderButton={(selectedItem) => (
+                                    <View className='flex-row items-center justify-center h-[56px] w-[112px] rounded-xl'>
+                                        <Text>{selectedItem?.flag ?? "🌎"}</Text>
+                                        <Text className='ml-2 text-[15px]'>{selectedItem?.code ?? "Selecione"}</Text>
+                                    </View>
+                                )}
+                                renderItem={(item) => (
+                                    <View className='flex-row items-center justify-center h-[35px]'>
+                                        <Text>{item.flag}</Text>
+                                        <Text className='ml-2 text-[15px]'>{item.code}</Text>
+                                    </View>
+                                )}
+                            />
                         </View>
                         
                         <View className='mt-[16px] mb-[16px]'>
                             <ArrowDownUp color="#0F172A"/>
                         </View>
 
-                        <View className='w-full h-[56px] rounded-xl border border-gray-100'>
-
+                        <View className='w-full h-[56px] p-[16px] rounded-xl border border-gray-300'>
+                            <Divider />
                         </View>
+
                     </View>
                 </View>
             </View>
